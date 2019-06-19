7a59bbd4d4cf2830c6ba63d923fd905412b4f999
Daniela Ortenzi <daniela.ortenzi92@gmail.com>
2019-06-19 13:49:18 +0200

    search page, controller & route

--- app/Http/Controllers/ApartmentController.php
+++ app/Http/Controllers/ApartmentController.php
@@ -5,25 +5,46 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Apartment;
 use App\Sponsorship;
+use App\Service;
 
 class ApartmentController extends Controller
 {
  public function show($id)
-  {    
+  {
     $apartment = Apartment::findOrFail($id);
         //   dd($apartment);
           return view('page.show-apartment-id', compact('apartment'));
   }
-  
-  
-  
+
+
+
   public function showSponsored(){
     $sponsoreds = Sponsorship::all();
       return view('page.sponsored-apartment', compact('sponsoreds'));
 
-      
+
+  }
+
+  public function search(Request $request){
+
+   $title = $request -> title;
+   $description = $request -> description;
+   $service = $request-> service;
+   $query = Apartment::query();
+
+   if ($title) {
+      $query = $query ->where('title', 'LIKE', '%' . $title . '%');
+    }
+    if ($description) {
+      $query = $query ->where('description', 'LIKE', '%' . $description . '%');
+    }
+
+
+    $apartments = $query ->get();
+
+    $services = Service::all();
+
+    return view('page.search', compact( 'apartments','services'));
+
   }
 }
-
-
-// @foreach ($post->categories as $category)
